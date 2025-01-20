21b91b355fe91ddf792572bf4c393b5dfe6d1d05
DESKTOP-6I6OMP3\123 <kanaeva.d.08@mail.ru>
2025-01-20 15:25:45 +0300

    end

--- handlers/duration_handler.go
+++ handlers/duration_handler.go
@@ -4,6 +4,8 @@ import (
 	"SFE_asynchronous_service/utils"
 	"net/http"
 
+	"log"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,8 +18,19 @@ func SetDuration(c *gin.Context) {
 		return
 	}
 
-	// Генерируем случайную продолжительность (1-30)
-	duration := utils.GenerateRandomDuration()
+	durationChan := make(chan int)
+
+	go func() {
+
+		duration := utils.GenerateRandomDuration()
+
+		log.Printf("Generated duration for pk %s: %d", pk, duration)
+
+		durationChan <- duration
+
+	}()
+
+	duration := <-durationChan
 
 	// Отправляем результат обратно в виде JSON
 	c.JSON(http.StatusOK, gin.H{
--- utils/random.go
+++ utils/random.go
@@ -7,6 +7,7 @@ import (
 
 // GenerateRandomDuration генерирует случайное число от 1 до 30
 func GenerateRandomDuration() int {
+	time.Sleep(5 * time.Second)
 	rand.Seed(time.Now().UnixNano()) // Инициализируем генератор случайных чисел
 	return rand.Intn(30) + 1         // Возвращаем число от 1 до 30
 }
